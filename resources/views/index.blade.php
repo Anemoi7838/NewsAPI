0287fcf1aff2b10ba6abcf1aa6bcbe1a6bbaeaee
Anemoi7838 <tm05247838@gmail.com>
2021-08-02 12:08:08 +0900

    better

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,8 @@
         <meta charset="utf-8">
         <meta http-equiv="X=UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, intial-scale=1">
+        <meta http-equiv="Content-Language" content="en">
+        <meta name="google" content="notranslate">
         <title>News</title>
         <link rel="stylesheet"
         href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
