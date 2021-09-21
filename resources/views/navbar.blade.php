0577eaf65bcddc02c9000b7f847c27161b69ffee
Anemoi7838 <tm05247838@gmail.com>
2021-09-21 13:32:51 +0900

    change navbar

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-inverse navbar-fixed-top">
+<!---<nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed"
@@ -21,9 +21,32 @@
             <input type="submit" name="category" value="sports">
             <input type="submit" name="category" value="technology">
         </form>
-        <li class="nav-item">
-            <a class="nav-link" href="#">データ一覧</a>
-        </li>
         </div>
     </div>
-</nav>
+</nav>--->
+<nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                        data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="/home">News </a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <form method="GET" action="/category">
+                <input type="submit" name="category" value="business">
+                <input type="submit" name="category" value="entertainment">
+                <input type="submit" name="category" value="general">
+                <input type="submit" name="category" value="health">
+                <input type="submit" name="category" value="science">
+                <input type="submit" name="category" value="sports">
+                <input type="submit" name="category" value="technology">
+            </form>
+            </div>
+        </div>
+    </nav>
